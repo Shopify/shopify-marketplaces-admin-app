b70215cc035514db41691929e3fa5671044e4125
Kristian Matthews <kristian.matthews@me.com>
2022-09-29 15:15:18 +0100

    Fix use environment variable config

--- models/index.js
+++ models/index.js
@@ -10,7 +10,11 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config);
+  if (process.env[config.use_env_variable]) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    sequelize = new Sequelize(config);
+  }
 } else {
   sequelize = new Sequelize(
     config.database,
